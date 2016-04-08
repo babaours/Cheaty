f1a70f588e437e80ce484335c1a4b8bd5a41d0b7
marchalh <marchal.hugues@outlook.com>
2016-04-08 19:14:12 +0200

    working on the adapter for the expandablelistview, modified textsize for list items

---
+++ app/src/main/java/hugues/marchal/cheaty/Activities/WifiScanActivity.java
@@ -0,0 +1,15 @@
+package hugues.marchal.cheaty.Activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import hugues.marchal.cheaty.R;
+
+public class WifiScanActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wifi_scan);
+    }
+}
